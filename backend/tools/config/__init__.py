bbb1088351fd2def8bb1edd3e39728577870fd04
asierballdom-hiberus <aballesteros@hiberus.com>
2026-03-25 13:55:52 +0100

    update classes

---
+++ backend/tools/config/__init__.py
@@ -0,0 +1,16 @@
+from .destinations_schema import DestinoEncontrado, RespuestaBusqueda, DestinoFinal, ListaDestinos
+from .FlightOption import FlightOption
+from .HotelOption import HotelOption
+from .TravelFinancialOption import TravelFinancialOption
+from .FinancialAgentResponse import FinancialAgentResponse
+
+__all__ = [
+    "DestinoEncontrado",
+    "RespuestaBusqueda",
+    "DestinoFinal",
+    "ListaDestinos",
+    "FlightOption",
+    "HotelOption",
+    "TravelFinancialOption",
+    "FinancialAgentResponse"
+]
