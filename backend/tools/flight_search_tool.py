8c4074d37d9a5856025ba477217c756027e7b89f
asierballdom-hiberus <aballesteros@hiberus.com>
2026-03-25 12:39:44 +0100

    add initial tool functions

---
+++ backend/tools/flight_search_tool.py
@@ -0,0 +1,8 @@
+from strands import tool
+
+@tool
+def search_flights(origin: str, destination: str, departure_date: str, return_date: str) -> str:
+    """
+    search for flights between origin and destination on the given dates and return a summary of the results.
+    """
+    pass
---
+++ backend/tools/hotel_search_tool.py
@@ -0,0 +1,8 @@
+from strands import tool
+
+@tool
+def search_hotels(destination: str, check_in_date: str, check_out_date: str) -> List[HotelOption]:
+    """
+    search for hotels in the destination on the given dates and return a summary of the results.
+    """
+    pass
